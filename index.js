5a20bb8ea90a867cc11ac35e4301c035b0321b5a
mdabarik <mdabarik19@gmail.com>
2023-12-08 17:57:17 +0600

    code reorganized

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
-require('dotenv').config();
 const app = express();
-const port = process.env.PORT || 5555
+require('dotenv').config();
 
-// middleware
-app.use(cors())
+const PORT = process.env.PORT || 5901
+
+/* middleware */
+app.use(cors());
 app.use(express.json());
 
-/*** Database Code */
-// hAjGF5PZAIr0FSz1 coffeeMaster
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.waijmz7.mongodb.net/?retryWrites=true&w=majority`;
+app.get('/', (req, res) => {
+    res.send('Server is running...');
+})
+
+/************ MongoDB Database User Start **************/
+const DB_USER="BrandShopUser"
+const DB_PASSWORD="03e5pzQcoLa999P5"
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.waijmz7.mongodb.net/?retryWrites=true&w=majority`;
+
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
@@ -23,92 +31,125 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        const brandCollection = client.db('BrandShopDB').collection('brands');
+        const prodTypesCollection = client.db('BrandShopDB').collection('productTypes');
+        const productCollection = client.db('BrandShopDB').collection('products');
+        const myCartCollectionsOnMongoDB = client.db('BrandShopDB').collection('cartsdata');
+        const sliderCollection = client.db('BrandShopDB').collection('slidersInfo');
+        const teamCollection = client.db('BrandShopDB').collection('teams');
 
-        const coffeCollection = client.db('CoffeeDB').collection('coffee');
-        const userCollection = client.db('CoffeeDB').collection('user');
 
-        app.get('/all', async (req, res) => {
-            const cursor = coffeCollection.find();
+        app.get("/brands", async (req, res) => {
+            const cursor = brandCollection.find();
             const result = await cursor.toArray();
             res.send(result);
         })
 
-        app.post('/coffee', async (req, res) => {
-            const newCoffee = req.body;
-            console.log(newCoffee);
-            const result = await coffeCollection.insertOne(newCoffee);
+        app.get("/product-types", async (req, res) => {
+            const cursor = prodTypesCollection.find();
+            const result = await cursor.toArray();
             res.send(result);
         })
 
-        app.delete("/coffee/:id", async (req, res) => {
+        app.get("/products", async (req, res) => {
+            const cursor = productCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+        app.get("/products/:id", async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
-            const result = await coffeCollection.deleteOne(query);
+            const result = await productCollection.findOne(query);
             res.send(result);
         })
-
-        // user related api
-        app.get('/user', async (req, res) => {
-            const cursor = userCollection.find();
-            const users = await cursor.toArray();
-            res.send(users);
-        })
-        app.post('/user', async (req, res) => {
-            const user = req.body;
-            console.log(user);
-            const result = await userCollection.insertOne(user);
+        app.post('/newprod', async (req, res) => {
+            const newProduct = req.body;
+            const result = await productCollection.insertOne(newProduct);
             res.send(result);
         })
-        app.delete('/user/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) };
-            const result = await userCollection.deleteOne(query);
-            res.send(result);
-        })
-        app.patch('/user', async (req, res) => {
-            const user = req.body;
-            const filter = { email: user.email };
-            const updatedUser = {
-                $set: {
-                    lastLoggedAt: user.lastLoggedAt
-                }
-            }
-            const result = await userCollection.updateOne(filter, updatedUser)
-            res.send(result)
-        })
-
-
-        app.get('/coffee/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) };
-            const result = await coffeCollection.findOne(query);
-            res.send(result);
-        })
-
-        app.put('/coffee/:id', async (req, res) => {
+        app.put('/edit/:id', async (req, res) => {
             const id = req.params.id;
             const filter = { _id: new ObjectId(id) };
             const options = { usert: true };
-            const updatedCoffee = req.body;
-            const coffee = {
+            const updateProduct = req.body;
+            const product = {
                 $set: {
-                    name: updatedCoffee.name,
-                    quantity: updatedCoffee.quantity,
-                    suppiler: updatedCoffee.suppiler,
-                    taste: updatedCoffee.taste,
-                    category: updatedCoffee.category,
-                    details: updatedCoffee.details,
-                    photoURL: updatedCoffee.photoURL
+                    productName: updateProduct.productName,
+                    brandName: updateProduct.brandName,
+                    price: updateProduct.price,
+                    brandType: updateProduct.brandType,
+                    shortDesc: updateProduct.shortDesc,
+                    rating: updateProduct.rating,
+                    photoURL: updateProduct.photoURL
                 }
             }
-            const result = await coffeCollection.updateOne(filter, coffee, options);
+            const result = await productCollection.updateOne(filter, product, options);
+            res.send(result);
+        })
+
+
+        /******* Cart Collection  Start ***********/
+        app.put('/mycart', async (req, res) => {
+            const receivedDataFromClient = req.body;
+
+            // my query we need two data to compare so we need $and (front mongo crud docs)
+            const myPutQuery = {
+                $and: [
+                    { productID: receivedDataFromClient.productID },
+                    { userEMAIL: receivedDataFromClient.userEMAIL }
+                ]
+            };
+
+            const options = { upsert: true }; // upsert
+
+            // new cart using $set
+            const newCartToBeInserted = {
+                $set: { 
+                    productID: receivedDataFromClient.productID,
+                    userEMAIL: receivedDataFromClient.userEMAIL }
+            }
+
+            const result = await myCartCollectionsOnMongoDB.updateOne(myPutQuery, newCartToBeInserted, options);
+            res.send(result);
+        })
+
+        app.get('/mycart', async (req, res) => {
+            const cursorPointer = myCartCollectionsOnMongoDB.find();
+            const result = await cursorPointer.toArray();
+            res.send(result);
+        })
+
+        app.delete('/mycart', async (req, res) => {
+            const receivedDataFromClient = req.body;
+            console.log(receivedDataFromClient);
+            // my query to be inserted
+            const myQuery = {
+                $and: [
+                    { userEMAIL: receivedDataFromClient.userEMAIL },
+                    { productID: receivedDataFromClient.productID }
+                ]
+            }
+            const result = await myCartCollectionsOnMongoDB.deleteOne(myQuery);
+            res.send(result);
+        })
+        /******* Cart Collection  End ***********/
+
+        // BrandShopDB.slidersInfo
+            app.get('/sliders', async (req, res) => {
+            const cursor = sliderCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        // BrandShopDB.teams
+        app.get('/teams', async (req, res) => {
+            const cursor = teamCollection.find();
+            const result = await cursor.toArray();
             res.send(result);
         })
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
@@ -116,13 +157,15 @@ async function run() {
     }
 }
 run().catch(console.dir);
-/****Database code ******/
+/************ MongoDB Database User Start **************/
 
-app.get('/', (req, res) => {
-    res.send('Coffee making server is running..')
+
+
+
+app.listen(PORT, () => {
+    console.log(`server is running at port: ${PORT}`);
 })
 
-app.listen(port, () => {
-    console.log(`coffee server is running on port: ${port}`);
-})
 
+// vercel
+// vercel --prod
